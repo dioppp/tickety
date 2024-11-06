f904f7b86307e74296728332269f2a1cb6147845
Frisaranda Diouf Julio <frisaranda@gmail.com>
2024-11-06 14:00:23 +0700

    add modernize

---
+++ resources/views/partials/logo-sidebar.blade.php
@@ -0,0 +1,3 @@
+<a href="../" class="text-nowrap">
+    <img src="{{ asset('images/logos/dark-logo.svg') }}" alt="Logo-Dark" />
+</a>
---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,6 @@
+<script src="{{ asset('node_modules/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('node_modules/simplebar/dist/simplebar.min.js') }}"></script>
+<script src="{{ asset('node_modules/iconify-icon/dist/iconify-icon.min.js') }}"></script>
+<script src="{{ asset('node_modules/@preline/dropdown/index.js') }}"></script>
+<script src="{{ asset('node_modules/@preline/overlay/index.js') }}"></script>
+<script src="{{ asset('js/sidebarmenu.js') }}"></script>
