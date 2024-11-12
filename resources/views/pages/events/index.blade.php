aa60208290d43a839beca9d27da3b14c26c85162
Frisaranda Diouf Julio <frisaranda@gmail.com>
2024-11-12 15:58:31 +0700

    minor update on index

--- resources/views/pages/events/index.blade.php
+++ resources/views/pages/events/index.blade.php
@@ -1,46 +1,41 @@
 @extends('partials.app')
 
 @section('content')
-    <main class="h-full overflow-y-auto  max-w-full  pt-4">
-        <div class="container full-container py-5 flex flex-col gap-6">
-            <div class="card-body">
-                <div class="grid grid-cols-1 xl:grid-cols-4 lg:gap-x-6 gap-x-0 lg:gap-y-6 gap-y-6">
-                    @foreach ($events as $event)
-                        <div class="flex flex-col gap-6">
-                            <div class="card overflow-hidden">
-                                <a href="{{ route('event.index') }}" class="bg-white">
-                                    @if ($event->image == 'default.jpg')
-                                        <img class="w-full h-auto rounded-t-xl" src="{{ asset('images/events/tulus.jpg') }}"
-                                            alt="Image Description">
-                                    @else
-                                        <img class="w-full h-auto rounded-t-xl"
-                                            src="{{ asset('storage/' . $event->image) }}" alt="Image Description">
-                                    @endif
-                                    <div class="card-body">
-                                        <h3 class="text-lg font-medium text-gray-700">
-                                            {{ ucwords($event->event_name) }}
-                                        </h3>
-                                        <p class="mt-1 text-sm text-gray-500 pr-10">
-                                            {{ \Carbon\Carbon::parse($event->start_date)->format('d M Y') }}
-                                        </p>
-                                        <h4 class="text-sm font-bold text-gray-700 mt-2">
-                                            {{ formatRupiah($event->cheapestTicketPrice()) }}
-                                        </h4>
-                                        <br>
-                                        <hr>
-                                        <a href="{{ route('dashboard') }}" class="flex items-center mt-4">
-                                            <img class="object-cover w-9 h-9 rounded-full"
-                                                src="{{ asset('images/profile/user-3.jpg') }}" alt=""
-                                                aria-hidden="true">
-                                            <span class="text-sm px-3">{{ ucfirst($event->user->name) }}</span>
-                                        </a>
-                                    </div>
+    <div class="card-body">
+        <div class="grid grid-cols-1 xl:grid-cols-4 lg:gap-x-6 gap-x-0 lg:gap-y-6 gap-y-6">
+            @foreach ($events as $event)
+                <div class="flex flex-col gap-6">
+                    <div class="card overflow-hidden">
+                        <a href="{{ route('event.index') }}" class="bg-white">
+                            @if ($event->image == 'default.jpg')
+                                <img class="w-full h-auto rounded-t-xl" src="{{ asset('images/events/tulus.jpg') }}"
+                                    alt="Image Description">
+                            @else
+                                <img class="w-full h-auto rounded-t-xl" src="{{ asset('storage/' . $event->image) }}"
+                                    alt="Image Description">
+                            @endif
+                            <div class="card-body">
+                                <h3 class="text-lg font-medium text-gray-700">
+                                    {{ ucwords($event->event_name) }}
+                                </h3>
+                                <p class="mt-1 text-sm text-gray-500 pr-10">
+                                    {{ \Carbon\Carbon::parse($event->start_date)->format('d M Y') }}
+                                </p>
+                                <h4 class="text-sm font-bold text-gray-700 mt-2">
+                                    {{ formatRupiah($event->cheapestTicketPrice()) }}
+                                </h4>
+                                <br>
+                                <hr>
+                                <a href="{{ route('dashboard') }}" class="flex items-center mt-4">
+                                    <img class="object-cover w-9 h-9 rounded-full"
+                                        src="{{ asset('images/profile/user-3.jpg') }}" alt="" aria-hidden="true">
+                                    <span class="text-sm px-3">{{ ucwords($event->user->name) }}</span>
                                 </a>
                             </div>
-                        </div>
-                    @endforeach
+                        </a>
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
-    </main>
+    </div>
 @endsection
