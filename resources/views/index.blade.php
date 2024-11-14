027e9313c60859d18a9bc8ae7252e6944c4d4a2c
Frisaranda Diouf Julio <frisaranda@gmail.com>
2024-11-14 14:59:41 +0700

    minor update on index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,16 +25,22 @@
         <h2 class="text-2xl font-medium text-gray-800 mb-6">Recommended For You</h2>
         <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
             @foreach ($events as $event)
-                <a href="" class="block bg-white shadow rounded overflow-hidden group">
+                <a href="{{ route('event.show', $event->id) }}" class="block bg-white shadow rounded overflow-hidden group" target="_blank">
                     <div class="relative">
-                        <img src="{{ asset('images/events/feast.jpg') }}" alt="product 1" class="w-full">
+                        @if ($event->image == 'default.jpg')
+                            <img class="w-full object-cover rounded-t-xl" src="{{ asset('images/events/tulus.jpg') }}"
+                                style="max-height: 180px" alt="Image Description">
+                        @else
+                            <img class="w-full object-cover rounded-t-xl" src="{{ asset('storage/' . $event->image) }}"
+                                style="max-height: 180px" alt="Image Description">
+                        @endif
                         <div
                             class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
                         </div>
                     </div>
                     <div class="pt-4 pb-3 px-4">
                         <h4 class="font-medium text-xl mb-2 text-gray-800">
-                            {{ $event->title }}
+                            {{ ucwords($event->event_name) }}
                         </h4>
                         <div class="flex items-baseline mb-1 space-x-2">
                             <p class="text-xl text-primary font-semibold">
