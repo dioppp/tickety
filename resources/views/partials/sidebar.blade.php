861e0ade321344199393d2f7a9b7b9850cd01816
Frisaranda Diouf Julio <frisaranda@gmail.com>
2024-12-18 13:38:18 +0700

    minor styling on sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -7,26 +7,26 @@
             <ul id="sidebarnav" class="text-gray-600 text-sm">
                 <li class="text-xs font-bold pb-4">
                     <i class="ti ti-dots nav-small-cap-icon text-lg hidden text-center"></i>
-                    <span>DISCOVER</span>
+                    <span>JELAJAH</span>
                 </li>
 
                 <li class="sidebar-item">
                     <a class="sidebar-link gap-3 py-2 px-3  rounded-md w-full flex items-center hover:text-blue-600 hover:bg-blue-500"
                         href="{{ route('event.index') }}">
-                        <i class="ti ti-calendar-event text-xl"></i> <span>Events</span>
+                        <i class="ti ti-calendar-event text-xl"></i> <span>Event</span>
                     </a>
                 </li>
 
                 <li class="sidebar-item">
                     <a class="sidebar-link gap-3 py-2 px-3  rounded-md w-full flex items-center hover:text-blue-600 hover:bg-blue-500"
                         href="{{ route('transaction.index') }}">
-                        <i class="ti ti-receipt text-xl"></i> <span>Transaction</span>
+                        <i class="ti ti-receipt text-xl"></i> <span>Transaksi</span>
                     </a>
                 </li>
 
                 <li class="text-xs font-bold mb-4 mt-8">
                     <i class="ti ti-dots nav-small-cap-icon text-lg hidden text-center"></i>
-                    <span>BACK TO HOME</span>
+                    <span>KEMBALI KE HALAMAN UTAMA</span>
                 </li>
 
                 <li class="sidebar-item">
