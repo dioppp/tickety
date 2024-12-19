36dbc19837219c93b737f0d1a627c9a98617b30f
Frisaranda Diouf Julio <frisaranda@gmail.com>
2024-12-19 19:17:51 +0700

    change logo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,9 +6,6 @@
                 <a href="">
                     <img src="{{ asset('images/logos/tickety-white.png') }}" alt="Logo" style="max-height: 50px">
                 </a>
-                {{-- <a href="{{ route('event.index') }}" class="text-gray-200 hover:text-white transition">Cek Event</a>
-                <a href="#" class="text-gray-200 hover:text-white transition">About us</a>
-                <a href="#" class="text-gray-200 hover:text-white transition">Contact us</a> --}}
             </div>
             @if (Auth::check())
                 <a href="{{ route('profile.index') }}"
